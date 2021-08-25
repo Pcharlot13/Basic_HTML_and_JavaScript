ddcfc05ab7b51107729077e4268f46d75ba2e44a
Pcharlot13 <43765007+Pcharlot13@users.noreply.github.com>
2021-08-25 16:38:40 -0400

    first assignment

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+var alertMessage = "This is project1_expressions_alert assignment";
+var firstText = "This is the first part of the expression,";
+var secondText = " this is the second part of the expression";   // These are the variables I will be using, all of them are strings
+
+window.alert(alertMessage); // this uses the 'alertMessage' variable and uses it's text to show an alert
+document.write(firstText + secondText); // This uses the firstText & secondText vars to display the text inside them.

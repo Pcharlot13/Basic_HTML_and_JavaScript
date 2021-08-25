b56048f88547035af30080ded323a5f5edca92b8
Pcharlot13 <43765007+Pcharlot13@users.noreply.github.com>
2021-08-25 16:48:09 -0400

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,9 @@
 var alertMessage = "This is project1_expressions_alert assignment";
 var firstText = "This is the first part of the expression,";
-var secondText = " this is the second part of the expression";   // These are the variables I will be using, all of them are strings
+var secondText = " this is the second part of the expression";   // These are the variables I will be using, all of them are strings// Using the var redText twice one to define the string and the other one to define the color
+var redText =" This message will be in red";
+var redText = redText.fontcolor(red); // the first part of redText defines the string and what it say the second part defines the font color
 
 window.alert(alertMessage); // this uses the 'alertMessage' variable and uses it's text to show an alert
 document.write(firstText + secondText); // This uses the firstText & secondText vars to display the text inside them.
+document.write(redText);

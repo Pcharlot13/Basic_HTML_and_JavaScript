05646e50485ed958d9824987ed7a80e5b123189c
Pcharlot13 <43765007+Pcharlot13@users.noreply.github.com>
2021-08-31 14:46:25 -0400

    assignment

--- Project4_Directories/main.js
+++ Project4_Directories/main.js
@@ -1,11 +1,11 @@
 function my_dictionary(){  // Initialized a function with different values for the given variable (dog), 
-    var animal={
+    var animal = {
         species:"dog",
         color:"black",
         breed:"labrador",
         Age:5,
         Sound:"bark!",
     };
-    delete Animal.Sound; // delete the sound value of the dog
+    delete animal.Sound; // delete the sound value of the dog
     document.getElementById("Dictionary").innerHTML = animal.sound; //displays the sound value 
 }

1affed99e676b613a4d78df7c6ff3accdcae1a08
Pcharlot13 <43765007+Pcharlot13@users.noreply.github.com>
2021-08-31 16:05:57 -0400

    assignment

---
+++ Project5_comparison_type_coersion/main.js
@@ -0,0 +1,47 @@
+function nan_test(){
+    document.getElementById("Test").innerHTML = 0/0;  // tests and display if NAN
+}
+function nan_test2(){
+    document.getElementById("Test").innerHTML = isNaN('This is a string'); // to test and display if the string is a NAN
+}
+function nan_test3(){
+    document.getElementById("Test").innerHTML= isNaN('20'); // test to display if string is a nan
+}
+function bool_test(){
+    var boolt = 3 > 5;  // result will be false, bool test
+    document.getElementById("Test").innerHTML= boolt;
+}
+function bool_test2(){
+    var boolt = 3 > 1;  // result will be true, bool test
+    document.getElementById("Test").innerHTML = boolt;
+}
+
+console.log(100 + 2); // simple math that will display on the console
+console.log(5 > 10); // bool to display on console
+
+function bool_eq(){
+    var boolt = (100==2);  // result will be false, equal bool test
+    document.getElementById("Test").innerHTML = boolt;
+}
+function bool_eq2(){
+    var boolt = (100==100);  // result will be true, equal bool test
+    document.getElementById("Test").innerHTML = boolt;
+}
+
+function eq_test(){
+    var boolt = (100===100); // will test if both type & value are equal and display
+    document.getElementById("Test").innerHTML = boolt;
+}
+function eq_test2(){
+    var boolt = ('this'===10); // will test if both type & value are equal and display
+    document.getElementById("Test").innerHTML = boolt;
+}
+function eq_test3(){
+    var boolt = ('10'===10); // will test if both type & value are equal and display
+    document.getElementById("Test").innerHTML = boolt;
+}
+function eq_test4(){
+    var boolt = (20 === 10); // will test if both type & value are equal and display
+    document.getElementById("Test").innerHTML = boolt;
+}
+

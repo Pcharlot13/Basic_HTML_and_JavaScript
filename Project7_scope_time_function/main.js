b9e0a7d7817985130d240bdc8dd18549da024648
Pcharlot13 <43765007+Pcharlot13@users.noreply.github.com>
2021-09-05 00:01:34 -0400

    update scope,time,function
    Competed If, Else, and else if assignmetns

--- Project7_scope_time_function/main.js
+++ Project7_scope_time_function/main.js
@@ -28,9 +28,25 @@ function get_Date(){
     if(new Date().getHours() > 18){     // returns a 'we are closed' statement if passed 6pm
         document.getElementById("Greeting").innerHTML="We are now closed, sorry.";
     }
+    // MY IF STATEMENT
+    if(new Date().getDate() < 15){      // uses the same function to let you know if you're on 1st or 2nd half of the week
+        document.getElementById("Week").innerHTML="Welcome to the first part of the week"
+    }
 }
-// MY IF STATEMENT
-function my_if(){
+
+// ELSE STATEMENTS
+function Age_Function(){
+    Age = document.getElementById("Age").value;
+    if(Age >= 18){
+        Vote = "You are old enough to vote!";
+    }
+    else {
+        Vote = "You are not enough to vote!";
+    }
+    document.getElementById("How_old_are_you?").innerHTML = Vote;
+}
+// MY ELSE STATEMENT
+function my_if(){       //The user will choose a number and if it is below or = to 7 it will be cool, other wise it's not a cool number
     var pickNumber = document.getElementById("Pick").value;
     if(pickNumber <= 7){
         document.getElementById("Cool").innerHTML = "You have chosen a cool number!"
@@ -38,4 +54,20 @@ function my_if(){
     else if(pickNumber > 7){
         document.getElementById("Cool").innerHTML = pickNumber + " is not a cool number, sorry."
     }
+}
+
+//ELSE IF STATEMENTS
+function Time_function(){      // this functions sets parameters for each portion for the day and tells you what day it is with Reply
+    var Time = new Date().getHours();
+    var Reply;
+    if(Time < 12 == Time > 0){
+        Reply = "It is morning time!";
+    }
+    else if(Time >= 12 == Time < 18){
+        Reply = "It is afternoon";
+    }
+    else{
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
 }

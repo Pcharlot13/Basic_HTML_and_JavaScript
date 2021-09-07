c61b455073e7d3a0d981df14af7632a12ee13aa5
Pcharlot13 <43765007+Pcharlot13@users.noreply.github.com>
2021-09-07 17:10:02 -0400

    tic tac toe update
    updated, not compelte, needs more debugging

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -4,7 +4,7 @@ let activePlayer = 'X';
 let selectedSquares = [];
 
 //This function is for placing an x or o in a square
-function placeXOro(squareNumber){
+function placeXOrO(squareNumber){
     //This condition ensures a square hasn't been selected already.
     //The .some() method is used to check each element of selecctedSquare array
     // to see if it contains the square number clicked on.
@@ -14,14 +14,14 @@ function placeXOro(squareNumber){
         //This condition checks who's turn it is
         if(activePlayer == 'X'){
             //If activePlayer is equal to 'X', the x.png is placed in html
-            select.style.backgroundImage = 'url(images/x.png)';
+            select.style.backgroundImage = 'url(./images/x.png)';
             //Active player may only be 'X' or '0' so, if not 'X' it must be 'O'
         } else{
             //if activePlayer is equal to 'O', the o.png is placed in html
-            select.style.backgroundImage='url(image/o.png)';
+            select.style.backgroundImage='url(./image/o.png)';
         }
         //squareNumber and activePlayer are concatenated togather and added to array.
-        selecctedSquare.push(squareNumber+activePlayer);
+        selectedSquares.push(squareNumber+activePlayer);
         //This callss a function to check for any win condition
         checkWinConditions();
         //This condition is for change the active plaer.
@@ -56,12 +56,70 @@ function placeXOro(squareNumber){
             //A random number between 0 and 8 is selected
             pickASquare = String(Math.floor(Math.random() * 9));
             //If the random number ecaluated returns true, the square hasn't been selected yet
-            if(placeXOro(pickASquare)){
+            if(placeXOrO(pickASquare)){
                 //This line calls he function
-                placeXOro(pickASquare);
+                placeXOrO(pickASquare);
                 //This changes our boolean and ends the loop
                 success = true;
             };
         }
     }
 }
+
+//This function parses the selectedSquares array to search for win conditions
+//drawWinLine function is called to draw line if condition is met
+function checkWinConditions(){
+    //X 0,1, 2 conditons
+    if      (arrayIncludes('0X','1X','2X')){ drawWinLine (50, 100,558, 100) }
+    //X 3,4, 5 condition
+    else if(arrayIncludes('3X', '4X', '5X')){drawWinLine(50, 304, 558, 304)}
+    // X 6, 7, 8 condition. 
+    else if (arrayIncludes ('6X', '7', '8X')) { drawWinLine (50, 508, 558, 508) }
+    // X0, 3, 6 condition. 
+    else if (arrayIncludes ('ox', '3', '6X')) { drawWinLine (100, 50, 100, 558) }
+    // XI, 4, 7 condition. 
+    else if (arrayIncludes ('iX', '4X', '7X')) { drawWinLine (304, 50, 304, 558) }
+    // X 2, 5, 8 condition. 
+    else if (arrayIncludes('2x', '5X', 'X')) { drawWinLine (508, 50, 508, 558) }
+    // X 6, 4, 2 condition. 
+    else if (arrayIncludes('6X', '4X', '2X')){drawWinLine (100, 508, 510, 90)}
+    // X 0, 4, 8 condition. 
+    else if (arrayIncludes ('ox', '4X', 'X')) { drawWinLine (100, 100, 520, 520) }
+    // O 0, 1, 2 condition. 
+    else if (arrayIncludes('00', '10', '20')) { drawWinLine (50, 100, 558, 100)}
+    // O 3, 4, 5 condition. 
+    else if (arrayIncludes ('30', '40', '50')) { drawWinLine (50, 304, 558, 304)}
+    // 06, 7, 8 condition. 
+    else if (arrayIncludes ('60', '70', '80')) { drawWinLine (50, 508, 558, 508) }
+    // 00, 3, 6 condition. 
+    else if (arrayIncludes ('00', '30', '60')) { drawWinLine (100, 50, 100, 558) } 
+    // 0i, 4, 7 condition.
+    else if (arrayIncludes ('10', '40', '70')) { drawWinLine (304, 50, 304, 558) }
+    // O 2, 5, 8 condition. 
+    else if (arrayIncludes ('20', '50', '80')) { drawWinLine (508, 50, 508, 558) }
+    // 06, 4, 2 condition. 
+    else if (arrayIncludes ('60', '40', '20')) { drawWinLine (100, 508, 510, 90) }
+    //00, 4, 8 condition. 
+    else if (arrayIncludes ('00', '40', '80')) { drawWinLine (100, 100, 520, 520) }
+    // This condition checks for tie. If none of the above conditions register and 9
+     // squares are selected the code executes. 
+     else if (selectedSquares.length >= 9) {
+         //This function playes the tie game sound. 
+         audio ('./media/tie.mp3'); 
+         // This function sets a .3 second timer before the resetGame is called. 
+         setTimeout(function () { resetGame(); }, 1000);
+     }
+
+    // This function checks if an array includes 3 strings. It is used to check for 
+    //each win condition. 
+    function arrayIncludes (squareA, squares, squarec) {
+        // These 3 variables will be used to check for 3 in a row. 
+        const a = selectedSquares.includes (squareA) 
+        const b = selectedSquares.includes (squareB) 
+        const c = selectedSquares.includes (squarec) 
+        // If the 3 variables we pass are all included in our array true is 
+        //returned and our else if condition executes the drawWinLine function. 
+        if (a === true && b === true && c === true) { return true }
+    }
+}
+
